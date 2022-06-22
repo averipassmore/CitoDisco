4b994af6d0b21dec21e2908640c8d6977ac6f1f2
averinpassmore <averinpassmore@gmail.com>
2022-06-22 14:03:50 -0700

    finished laying out meet the team component

--- src/components/MeetTheTeam.js
+++ src/components/MeetTheTeam.js
@@ -1,12 +1,27 @@
 import React from "react";
+import Testimonials from "./Testimonials";
 
 const MeetTheTeam = () => {
   return (
     <div>
       <div className="Meet-the-team">
         <h1>Meet The Team</h1>
-        {/* <p>y</p> */}
+        <div className="Bio">
+          <img src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="Laura"></img>
+          <p>Curabitur vel lacus non tellus ullamcorper varius in lobortis magna. Cras maximus vitae nibh id bibendum. Donec vehicula at libero et rutrum. Donec gravida sem tellus, sed condimentum velit ultricies vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam et vestibulum arcu. Phasellus eu pretium arcu. Duis ut ex a justo sagittis pretium sit amet ac leo. Maecenas sit amet justo sed massa consequat malesuada quis eu eros. Maecenas laoreet lorem vitae tincidunt cursus. Curabitur id neque auctor elit ornare auctor at in odio. Quisque vel varius enim, non iaculis urna. Phasellus quis metus ut enim tincidunt dapibus. Sed egestas sem cursus urna ultrices, a dignissim dolor ultricies.</p>
+        </div>
+        <div className="Non-owners">
+          <div className="Bio">
+            <img src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="Analisa"></img>
+            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam et vestibulum arcu. Phasellus eu pretium arcu. Duis ut ex a justo sagittis pretium sit amet ac leo. Maecenas sit amet justo sed massa consequat malesuada quis eu eros. Maecenas laoreet lorem vitae tincidunt cursus. Curabitur id neque auctor elit ornare auctor at in odio. Quisque vel varius enim, non iaculis urna. Phasellus quis metus ut enim tincidunt dapibus. Sed egestas sem cursus urna ultrices, a dignissim dolor ultricies.</p>
+          </div>
+          <div className="Bio">
+            <img src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="Ruth"></img>
+            <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam et vestibulum arcu. Phasellus eu pretium arcu. Duis ut ex a justo sagittis pretium sit amet ac leo. Maecenas sit amet justo sed massa consequat malesuada quis eu eros. Maecenas laoreet lorem vitae tincidunt cursus. Curabitur id neque auctor elit ornare auctor at in odio. Quisque vel varius enim, non iaculis urna. Phasellus quis metus ut enim tincidunt dapibus. Sed egestas sem cursus urna ultrices, a dignissim dolor ultricies.</p>
+          </div>
+        </div>
       </div>
+      <Testimonials/>
     </div>
   )
 }
---
+++ src/components/Testimonials.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <div className="Testimonials">
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials;
