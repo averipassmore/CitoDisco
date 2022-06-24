c4f4891c0a6e98f19d529aaf9ada676fcf1cb21b
averinpassmore <averinpassmore@gmail.com>
2022-06-24 01:03:13 -0700

    footer layout complete

--- src/components/Supporting.js
+++ src/components/Supporting.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./Footer";
 
 const Supporting = () => {
   return (
@@ -14,6 +15,7 @@ const Supporting = () => {
           <img src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="Reviewer"></img>
         </div>
       </div>
+      <Footer/>
     </div>
   )
 }
