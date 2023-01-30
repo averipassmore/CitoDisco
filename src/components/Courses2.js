a4dc8ae0c515a322997de9b0776c41e68eb908ff
averinpassmore <averinpassmore@gmail.com>
2023-01-30 16:24:11 -0500

    added instagram link and courses images

--- src/components/Courses2.js
+++ src/components/Courses2.js
@@ -1,6 +1,10 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { useNavigate } from "react-router-dom";
+import italianCourse from "../images/italianCourse.jpg"
+import latinCourse from "../images/latinCourse.jpg"
+import japaneseCourse from "../images/japaneseCourse.jpg"
+import eslCourse from "../images/eslCourse.jpg"
 
 const Courses2 = () => {
   const navigate = useNavigate();
@@ -9,28 +13,20 @@ const Courses2 = () => {
     <div>
       <NavBar />
       <div style={{position: "relative", display: "flex", top: "8vmin", justifyContent: "center"}}>
-        <h1>Courses</h1>
+        <h1 style={{fontSize: "6vmin"}}>Courses</h1>
       </div>
       <div className="Courses-List">
         <div className="Course-Card" onClick={() => navigate('/courses/italian')}>
-          <h1>Italian</h1>
-          <h3>Italian For Adults</h3>
-          <h4>Online or in person!</h4>
-          <h3>Italian for Children</h3>
-          <h4>Online or in person!</h4>
-          <h2>See more!</h2>
+          <img src={italianCourse} alt={"Italian course"}></img>
         </div>
         <div className="Course-Card">
-          <h1>Latin</h1>
-          <h4>Coming soon...</h4>
+          <img src={latinCourse} alt={"Italian course"}></img>
         </div>
         <div className="Course-Card">
-          <h1>Japanese</h1>
-          <h4>Coming soon...</h4>
+          <img src={japaneseCourse} alt={"Italian course"}></img>
         </div>
         <div className="Course-Card">
-          <h1>ESL</h1>
-          <h4>Coming soon...</h4>
+          <img src={eslCourse} alt={"Italian course"}></img>
         </div>
       </div>
       <Footer/>
